00783a8fa56dff03b9923c7843c145a26d2d0ac4
MeguminFireFox <ambrehardy013@gmail.com>
2025-11-25 12:19:58 +0100

    D'autre boisson

--- Project1/Project1/Drink.hpp
+++ Project1/Project1/Drink.hpp
@@ -6,12 +6,17 @@ class Drink {
 public:
     virtual ~Drink() = default;
     virtual std::vector<std::string> getSteps() const = 0;
+    virtual int getPrice() const = 0;
 };
 
 class PumpkinLatte : public Drink {
 public:
     std::vector<std::string> getSteps() const override {
         return { "cafe", "lait", "epices", "pumpkin" };
+    }
+
+    int getPrice() const override {
+        return 5;
     }
 };
 
@@ -20,18 +25,63 @@ public:
     std::vector<std::string> getSteps() const override {
         return { "eau", "herbe" };
     }
+
+    int getPrice() const override {
+        return 1;
+    }
 };
 
-class VoidCoffe : public Drink {
+class VoidCoffee : public Drink {
 public:
     std::vector<std::string> getSteps() const override {
         return { "" };
     }
+
+    int getPrice() const override {
+        return 2;
+    }
 };
 
-/*class VoidCoffe : public Drink {
+class Coffee : public Drink {
 public:
     std::vector<std::string> getSteps() const override {
-        return { "" };
+        return { "cafe", "eau" };
     }
-};*/
+
+    int getPrice() const override {
+        return 3;
+    }
+};
+
+class CiaoKombucha : public Drink {
+public:
+    std::vector<std::string> getSteps() const override {
+        return { "eau", "sucre", "the", "kombucha"};
+    }
+
+    int getPrice() const override {
+        return 15;
+    }
+};
+
+class BubbleTea : public Drink {
+public:
+    std::vector<std::string> getSteps() const override {
+        return { "perle de tapioca", "the", "lait", "sucre" };
+    }
+
+    int getPrice() const override {
+        return 10;
+    }
+};
+
+class HotChocolate : public Drink {
+public:
+    std::vector<std::string> getSteps() const override {
+        return { "lait", "chocolat", "sucre"};
+    }
+
+    int getPrice() const override {
+        return 4;
+    }
+};
